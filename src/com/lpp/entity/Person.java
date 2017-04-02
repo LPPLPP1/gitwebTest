45c4fa318d1a948705f505aeeeb0ab4757faa18f
LPPLPP1 <15207111258@163.com>
2017-04-02 12:49:20 +0800

    修改一

---
+++ src/com/lpp/entity/Person.java
@@ -0,0 +1,19 @@
+package com.lpp.entity;
+
+public class Person {
+private String namep;
+private int age;
+public String getNamep() {
+	return namep;
+}
+public void setNamep(String namep) {
+	this.namep = namep;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+}
